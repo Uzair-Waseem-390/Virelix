10543b518e49b0127d4c67a531a9f04235b036f0
Uzair-Waseem-390 <uzairwaseem390@gmail.com>
2026-03-26 22:14:09 +0500

    database setup done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,11 +64,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('ENGINE'),
+        'NAME': os.getenv("NAME"),    
+        'USER': os.getenv("USER"),   
+        'PASSWORD': os.getenv("PASSWORD"),    
+        'HOST': os.getenv("HOST"),      
+        'PORT': os.getenv("PORT")       
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
