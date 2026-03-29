bb39c69bce3fc2a709d82d7494ca4bf1c4d094dd
Uzair-Waseem-390 <uzairwaseem390@gmail.com>
2026-03-29 18:28:10 +0500

    frontend setup with Tailwind Working

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
